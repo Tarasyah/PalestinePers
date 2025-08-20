f2587206aebdd34c7cce0f4e1a32a91a4f6ced0b
Skat Skadi <skatskadi17@gmail.com>
2025-08-20 05:37:37 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Card className="p

--- src/components/gaza-tracker.tsx
+++ src/components/gaza-tracker.tsx
@@ -35,14 +35,14 @@ function GazaTracker() {
     return (
       <div className="space-y-4">
         <Skeleton className="h-6 w-3/4" />
-        <Skeleton className="h-4 w-full" />
-        <div className="space-y-2">
+        <div className="space-y-2 pt-2">
           <Skeleton className="h-5 w-5/6" />
           <Skeleton className="h-5 w-4/6" />
           <Skeleton className="h-5 w-5/6" />
         </div>
-        <Skeleton className="w-full aspect-video" />
-        <Skeleton className="w-full aspect-video" />
+        <Skeleton className="w-full aspect-video rounded-lg" />
+        <Skeleton className="w-full aspect-video rounded-lg" />
+        <Skeleton className="h-4 w-full mt-2" />
       </div>
     );
   }
@@ -55,10 +55,9 @@ function GazaTracker() {
     <div className="gaza-tracker-stats space-y-4">
       <div>
         <h4 className="font-bold text-lg text-white">Gaza Casualty Tracker</h4>
-        <p><small className="text-muted-foreground">Source: Palestinian Ministry of Health in Gaza as of {new Date(tracker.update_date).toLocaleDateString()}</small></p>
       </div>
       
-      <ul className="space-y-1 list-disc list-inside text-white">
+      <ul className="space-y-1 list-disc list-inside text-white text-sm">
         <li><strong>Confirmed killed:</strong> {tracker.confirmed_killed?.toLocaleString()} (incl. {tracker.children_killed?.toLocaleString()} children)</li>
         <li><strong>Injured:</strong> {tracker.injured?.toLocaleString()}</li>
         <li><strong>Starvation Deaths:</strong> {tracker.starvation_deaths} (incl. {tracker.children_starvation_deaths} children)</li>
@@ -66,14 +65,15 @@ function GazaTracker() {
 
       {tracker.deaths_chart_url && (
         <div className="relative w-full aspect-video">
-           <Image src={tracker.deaths_chart_url} alt="Gaza death tracker chart" layout="fill" objectFit="contain" className="rounded-lg" />
+           <Image src={tracker.deaths_chart_url} alt="Gaza death tracker chart" layout="fill" objectFit="contain" className="rounded-lg border border-white/10" />
         </div>
       )}
       {tracker.famine_chart_url && (
         <div className="relative w-full aspect-video">
-          <Image src={tracker.famine_chart_url} alt="Gaza famine tracker chart" layout="fill" objectFit="contain" className="rounded-lg" />
+          <Image src={tracker.famine_chart_url} alt="Gaza famine tracker chart" layout="fill" objectFit="contain" className="rounded-lg border border-white/10" />
         </div>
        )}
+       <p><small className="text-muted-foreground text-xs">Source: Palestinian Ministry of Health in Gaza as of {new Date(tracker.update_date).toLocaleDateString()}</small></p>
     </div>
   );
 }
