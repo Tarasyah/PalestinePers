5fcc78654fb88d376e1855e5f316257f066e4050
Skat Skadi <skatskadi17@gmail.com>
2025-08-21 22:55:47 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <h2 class="text-2x

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -8,7 +8,7 @@ export default function MediaPage() {
   return (
     <AppLayout>
         <div className="space-y-4">
-            <h2 className="text-2xl font-bold tracking-tight text-center">A chapter we can never forget.</h2>
+            <h2 className="text-2xl font-bold tracking-tight text-center">A chapter we can never forget</h2>
             <div className="flex flex-wrap justify-center items-center gap-4">
                 {mediaItems.map((item: MediaItem) => (
                     <MediaCard key={item.id} item={item} />
